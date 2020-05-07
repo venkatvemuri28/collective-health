9a84b29119c82c97da6c88e66bf43e16e84f4146
Venkata Vemuri <padma.vemuri@gmail.com>
2020-05-07 08:53:06 -0700

    test commit from localhost

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
-import "./styles.css";
+import React from 'react';
+import './styles.css';
 
 export default function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Hello CodeSandbox & Git hub</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Start editing to see some magic happen! From visual studio</h2>
     </div>
   );
 }
 
-export const avg = arr => arr.reduce((a, c) => a + c) / arr.length;
+export const avg = (arr) => arr.reduce((a, c) => a + c) / arr.length;
